5005f8e6f2be40c1b2878f1157dfeb6ea6393c1b
nagdista <nagdista@gmail.com>
2025-06-09 12:27:41 +0300

    fix bug in slug

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default async function SinglePage({params}:{params:{slug:string}}) {
-  const slugName = params.slug ;
+export default function SinglePage({ params }: any) {
+  const slugName = params.slug;
   return (
-    <div className='h-[calc(100vh-120px)] w-full flex justify-center items-center text-3xl'>Single Page | {slugName} Page </div>
-  )
+    <div className="h-[calc(100vh-120px)] w-full flex justify-center items-center text-3xl">
+      Single Page | {slugName} Page
+    </div>
+  );
 }
