b8836734ffb1ded799c2bce0fc925da44cc4d178
nagdista <nagdista@gmail.com>
2025-06-09 12:21:34 +0300

    make component async for params

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function SinglePage({params}:{params:{slug:string}}) {
+export default async function SinglePage({params}:{params:{slug:string}}) {
   const slugName = params.slug ;
   return (
     <div className='h-[calc(100vh-120px)] w-full flex justify-center items-center text-3xl'>Single Page | {slugName} Page </div>
