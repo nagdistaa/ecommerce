925a56ebc193fc22bcbc708121f04e6cf6bad39a
nagdista <nagdista@gmail.com>
2025-06-09 10:08:10 +0300

    fix footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ export default function RootLayout({
       <body className="inter.className">
        <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='flex justify-center items-center w-full '>Made By ❤️| All Rigt Recieved &copy; 2025</div>
   )
 }
