adf915fb6f9b91cb0cdb7a385c8c998474b2d7f6
nagdista <nagdista@gmail.com>
2025-06-09 12:33:47 +0300

    fix buggggggggg

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 
-export default function SinglePage({ params }: { params: { slug: string } }) {
-  const slugName = params.slug;
+interface Props {
+  params: {
+    slug: string;
+  };
+}
+
+export default function SinglePage({ params }: Props) {
+  const { slug } = params;
+
   return (
     <div className="h-[calc(100vh-120px)] w-full flex justify-center items-center text-3xl">
-      Single Page | {slugName} Page
+      Single Page | {slug} Page
     </div>
   );
 }
